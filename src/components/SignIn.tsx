e7966ceb93864e47ad6ba2357303d017ec658ef5
krsnamara <martinj.fitzpatrick@gmail.com>
2024-03-13 17:03:28 -0400

    Add google icon for signin and Build out logic for signIn using NextAuth

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link'
 import { Icons } from './Icons'
+import UserAuthForm from './UserAuthForm'
+
 const SignIn = () => {
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w[400px] ">
@@ -8,6 +11,19 @@ const SignIn = () => {
         <p className="text-sm max-w-xs mx-auto">
           By continuing, you are setting up a Breadit account and agree to our
           User Agreement and Privacy Policy.
+        </p>
+
+        {/* TODO sign in form */}
+        <UserAuthForm />
+
+        <p className="px-8 text-center text-sm text-zinc-700">
+          New to Breadit?{' '}
+          <Link
+            href="/sign-up"
+            className="hover:text-zinc-800 text-sm underline underline-offset-4"
+          >
+            Sign up
+          </Link>
         </p>
       </div>
     </div>
