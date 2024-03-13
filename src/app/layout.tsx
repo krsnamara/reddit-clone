c95bb509ae80799ad48ee0a6d0cc02f3b6434b2f
krsnamara <martinj.fitzpatrick@gmail.com>
2024-03-13 14:12:13 -0400

    Import Navbar from components and render in return

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/ui/Navbar'
 
 export const metadata = {
   title: 'Breadit',
@@ -23,7 +24,7 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        {/* <Navbar /> */}
+        <Navbar />
 
         <div className="container max-w-7xl mx-auto h-full pt-12 ">
           {children}
