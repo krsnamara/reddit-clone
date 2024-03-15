8e359048b6fcfc4d65cc4631c00133d336e54088
krsnamara <martinj.fitzpatrick@gmail.com>
2024-03-14 22:17:38 -0400

    Build out the Avatar rendering of user profile data with conditional for users without profile images

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,12 +1,31 @@
 import { User } from 'next-auth'
 import { FC } from 'react'
+import { Avatar, AvatarFallback } from './ui/avatar'
+import Image from 'next/image'
+import { Icons } from './Icons'
 
 interface UserAvatarProps {
   user: Pick<User, 'name' | 'image'>
 }
 
 const UserAvatar: FC<UserAvatarProps> = ({ user }) => {
-  return <div>UserAvatar</div>
+  return (
+    <Avatar>
+      {user.image ? (
+        <Image
+          fill
+          src={user.image}
+          alt="profile picture"
+          referrerPolicy="no-referrer"
+        />
+      ) : (
+        <AvatarFallback>
+          <span className="sr-only">{user?.name}</span>
+          <Icons.user className="h-4 w-4" />
+        </AvatarFallback>
+      )}
+    </Avatar>
+  )
 }
 
 export default UserAvatar
