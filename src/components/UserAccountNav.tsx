0a9ed0e1369e8d3b2fe5b40f575d858834a779f8
krsnamara <martinj.fitzpatrick@gmail.com>
2024-03-15 09:28:56 -0400

    Continue to build out dropdown menu and add sign out functionality

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,7 +1,17 @@
+'use client'
+
 import { User } from 'next-auth'
 import { FC } from 'react'
-import { DropdownMenu, DropdownMenuTrigger } from './ui/DropdownMenu'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/DropdownMenu'
 import UserAvatar from './UserAvatar'
+import Link from 'next/link'
+import { signOut } from 'next-auth/react'
 
 interface UserAccountNavProps {
   user: Pick<User, 'name' | 'email' | 'image'>
@@ -11,17 +21,50 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <UserAvatar user={user} />
+        <UserAvatar
+          className="h-8 w-8"
+          user={{ name: user.name || null, image: user.image || null }}
+        />
       </DropdownMenuTrigger>
-      {/* <DropdownMenu.Content>
-      <DropdownMenu.Group>
-        <DropdownMenu.Item>Profile</DropdownMenu.Item>
-        <DropdownMenu.Item>Settings</DropdownMenu.Item>
-      </DropdownMenu.Group>
-      <DropdownMenu.Group>
-        <DropdownMenu.Item>Sign Out</DropdownMenu.Item>
-      </DropdownMenu.Group>
-    </DropdownMenu.Content> */}
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-zinc-700">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href="/">Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/r/create">Create community</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/settings">Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onSelect={(event) => {
+            event.preventDefault()
+            signOut({
+              callbackUrl: `${window.location.origin}/sign-in`,
+            })
+          }}
+        >
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
