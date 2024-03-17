7e4971d525639e129f1bc33808eb247f513e4bdd
krsnamara <martinj.fitzpatrick@gmail.com>
2024-03-17 02:28:48 -0400

    Import navigation router. Add functionality to X button in modal returning user to previous page

--- src/components/CloseModal.tsx
+++ src/components/CloseModal.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import { X } from 'lucide-react'
 import { Button } from './ui/Button'
+import { useRouter } from 'next/navigation'
 
 const CloseModal = () => {
+  const router = useRouter()
   return (
-    <Button variant="subtle" aria-label="Close modal">
+    <Button
+      variant="subtle"
+      className="h-6 w-6 p-0 rounded-md"
+      onClick={() => router.back()}
+      aria-label="Close modal"
+    >
       <X className="w-4 h-4" />
     </Button>
   )
