d294db0d8626cf70a7d68c8ce077cc4dc1e95744
krsnamara <martinj.fitzpatrick@gmail.com>
2024-03-18 00:54:56 -0400

    Deletes 500 from status response message

--- src/app/api/subreadit/route.ts
+++ src/app/api/subreadit/route.ts
@@ -42,6 +42,6 @@ export async function POST(req: Request) {
       return new Response(error.message, { status: 422 })
     }
 
-    return new Response('Could not create subreadit 500', { status: 500 })
+    return new Response('Could not create subreadit', { status: 500 })
   }
 }
